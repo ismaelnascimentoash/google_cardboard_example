c659d25cbb5c788e995c669ce83bc056d156e9c5
Ismael Nascimento 'Ash <contato@ismaelnascimento.com>
2020-05-12 22:29:23 -0300

    [ADDED] Video 360

---
+++ Assets/_Project/Scripts/InvertNormal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace IsmaelNascimentoAssets
+{
+    public class InvertNormal : MonoBehaviour
+    {
+        private void Start()
+        {
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+            Vector3[] normalsInvert = mesh.normals;
+
+            for (int i = 0; i < normalsInvert.Length; i++)
+            {
+                normalsInvert[i] = -1 * normalsInvert[i];
+            }
+
+            mesh.normals = normalsInvert;
+
+            for (int i = 0; i < mesh.subMeshCount; i++)
+            {
+                int[] tris = mesh.GetTriangles(i);
+
+                for (int j = 0; j < tris.Length; j += 3)
+                {
+                    int temp = tris[j];
+                    tris[j] = tris[j + 1];
+                    tris[j + 1] = temp;
+                }
+
+                mesh.SetTriangles(tris, i);
+            }
+        }
+    }
+}
